3f93a329dac7ee46745738e4d6adbbfb6ebe4458
rtthiel8 <93964501+rtthiel8@users.noreply.github.com>
2022-04-07 19:01:08 -0500

    controllers

---
+++ controllers/user-controller.js
@@ -0,0 +1,38 @@
+const { User } = require('../models');
+
+const userController = {
+    // get all users
+    getAllUser(req, res) {
+        User.find({})
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            })
+    },
+    // get one user by id
+    getUserById({ params }, res) {
+        User.findOne({ _id: params.id })
+            .then(dbUserData => {
+                // if no user is found, send 404
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No user found with this id.' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+    // create user
+    createUser({ body }, res) {
+        User.create(body)
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => res.status(400).json(err));
+    },
+};
+
+module.exports = userController;
+
